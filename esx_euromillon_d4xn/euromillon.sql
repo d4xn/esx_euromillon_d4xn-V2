f8b195453225ecbe78cf2c47195e42c9b33805b4
d4xn <80895035+d4xn@users.noreply.github.com>
2021-06-01 11:59:34 +0200

    esx_euromillon_d4xn V2 First Commit

--- esx_euromillon_d4xn/euromillon.sql
+++
@@ -1,6 +0,0 @@
-/*
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('boleto_euromillon', 'Boleto Euromillon', 1, 0, 1)
-;
-	-- Change the label if you speak another language, and if u want change le limit or whatever
-*/
